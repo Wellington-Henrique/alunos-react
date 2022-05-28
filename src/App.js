26deec8a2428217a79f71466cddd0565b8425bb8
Wellington Henrique <wellingtonhlc@gmail.com>
2022-05-28 16:54:25 -0300

    Adding Post student

--- src/App.js
+++ src/App.js
@@ -6,13 +6,49 @@ import LogoCadastro from './assets/cadastro-ico.jpg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
+const baseUrl = "https://localhost:44311/api/";
+
+const initialState = {
+  id: '',
+  nome: '',
+  email: '',
+  idade: ''
+};
+
 function App() {
-  const baseUrl = "https://localhost:44311/api/alunos";
   const [data, setData] = useState([]);
+  const [alunoSelecionado, setAlunoSelecionado] = useState(initialState);
+  const [modalIncluir, setModalIncluir] = useState(false);
+
+  const abrirFecharModalIncluir = () => {
+    setModalIncluir(!modalIncluir);
+  }
+
+  const handleChange = e => {
+    const {name, value} = e.target;
+    setAlunoSelecionado({
+      ...alunoSelecionado, [name]:value
+    });
+    console.log(alunoSelecionado);
+  }
 
   const pedidoGet = async() => {
-    await axios.get(baseUrl)
+    await axios.get(`${baseUrl}alunos`)
     .then(response => setData(response.data))
+    .catch(error => {
+      console.log(error)
+    })
+  }
+
+  const pedidoPost = async() => {
+    delete alunoSelecionado.id;
+    alunoSelecionado.idade = parseInt(alunoSelecionado.idade);
+
+    await axios.post(`${baseUrl}alunos`, alunoSelecionado)
+    .then(response => {
+      setData(data.concat(response.data));
+      abrirFecharModalIncluir();
+    })
     .catch(error => {
       console.log(error)
     })
@@ -28,7 +64,7 @@ function App() {
 
       <header className="">
         <img src={LogoCadastro} alt="Cadastro"></img>
-        <button className="btn btn-success" onClick={() => console.log("Clicou...")}>Incluir Novo Aluno</button>
+        <button className="btn btn-success" onClick={() => abrirFecharModalIncluir()}>Incluir Novo Aluno</button>
       </header>
 
       <table className="table table-bordered">
@@ -56,6 +92,27 @@ function App() {
           ))}
         </tbody>
       </table>
+
+      <Modal isOpen={modalIncluir}>
+        <ModalHeader>Incluir Alunos</ModalHeader>
+        <ModalBody>
+          <div className="form-group">
+            <label>Nome:</label>
+            <br/>
+            <input type="text" className="form-control" name="nome" onChange={handleChange}/>
+            <label>Email:</label>
+            <br/>
+            <input type="text" className="form-control" name="email" onChange={handleChange}/>
+            <label>Idade:</label>
+            <br/>
+            <input type="text" className="form-control" name="idade" onChange={handleChange}/>
+          </div>
+        </ModalBody>
+        <ModalFooter>
+          <button className="btn btn-primary" onClick={() => pedidoPost()}>Incluir</button>
+          <button className="btn btn-danger" onClick={() => abrirFecharModalIncluir()}>Cancelar</button>
+        </ModalFooter>
+      </Modal>
     </div>
   );
 }
